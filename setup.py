e58c907c1d93fe4b68ed7d4071033105d68bc309
Chanpreet Singh <chanpreet3000@gmail.com>
2024-12-15 00:17:29 +0530

    version updated

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="cuttlepy",
-    version="0.1.0",
+    version="0.1.1",
     author="Chanpreet Singh",
     author_email="chanpreet3000@gmail.com",
     description="Typed python HTTP client wrapper for PRIMP",
